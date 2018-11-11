5207610ea03e8dc0b61355b0a6e6fe829f1fe664
joecosta117 <joecosta117@gmail.com>
2018-11-11 16:12:53 -0500

    had to update diceset model to add name

--- server/db/models/diceset.js
+++ server/db/models/diceset.js
@@ -2,6 +2,10 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const DiceSet = db.define('diceset', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
   dice: {
     type: Sequelize.STRING,
     allowNull: false
